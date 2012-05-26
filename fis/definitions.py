774e3de918718118f8ff8c0a5e181bd429dee2c1
Oscar Mederos Oceja <omederos@gmail.com>
2012-05-26 05:29:50 -0400

    Add 'definition' package
    - ValueDefinition
    - VariableDefinition
    - Variable
    - VariableCollection

---
+++ fis/definitions.py
@@ -0,0 +1,118 @@
+# -*- coding: utf-8 -*-
+from copy import copy
+
+
+class ValueDefinition(object):
+    """
+    Representa un valor linguistico de una variable
+
+    Guarda la siguiente informacion:
+    - Valor linguistico de una cierta variable
+    - Funciones de membresia de la variable para ese valor linguistico
+
+    """
+    def __init__(self, value, function):
+        self.value = value
+        self.function = function
+
+
+class VariableDefinition(object):
+    """
+    Representa la definicion de una variable
+
+    Contiene la siguiente informacion:
+    - Nombre de la variable (ej. Calidad, Propina, etc.)
+    - Valor de la variable (ver clase 'Value')
+
+    """
+    def __init__(self, name, values=None):
+        self.name = name
+        if not values:
+            values = []
+        self.values = values
+
+    def add_value(self, value):
+        self.values.append(value)
+
+    def get_value(self, value):
+        for v in self.values:
+            if v.value == value:
+                return v
+        return None
+
+    def __str__(self):
+        return 'Definition: %s %s' % (self.name, self.values)
+
+
+class Variable(object):
+    """
+    Representa una variable linguistica.
+
+    Esta clase es necesaria pues redefine los operadores &, | y - unario
+    Contiene la siguiente informacion:
+    - Definicion de la variable ('definition')
+    - Valor de la variable (ver clase 'Value')
+    - Valor obtenido al evaluar la funcion de membresia en el valor inicial
+      que tomó esta variable (ej. 0, 1, 0.2, etc.)
+
+    """
+    def __init__(self, definition, value, membership_value):
+        self.definition = definition
+        self.value = value
+        self.membership_value = membership_value
+
+    def __str__(self):
+        return '%s = %s (%s)' % (self.definition.name, self.value,
+                                 self.membership_value)
+
+    ### Redefinimos los operadores siguientes de acuerdo con la operación
+    ### correspondiente en las reglas de la lógica de difusa
+
+    def __and__(self, other):
+        d = {self.membership_value: self,
+             other.membership_value: other}
+        return d[min(self.membership_value, other.membership_value)]
+
+    def __or__(self, other):
+        d = {self.membership_value: self,
+             other.membership_value: other}
+        return d[max(self.membership_value, other.membership_value)]
+
+    def __neg__(self):
+        var = copy(self)
+        var.membership_value = 1 - self.membership_value
+        return var
+
+
+class VariableCollection(list):
+    """
+    Representa una lista de definiciones de variables linguisticas
+
+    """
+    def __init__(self, seq=()):
+        super(VariableCollection, self).__init__(seq)
+        self.input = {}
+
+    def get_var(self, variable, value):
+        """
+        Devuelve la variable linguistica 'variable' con valor ling. 'valor'
+
+        """
+        for var in self:
+            if var.name == variable:
+                function = var.get_value(value).function
+
+                membership_value = function.evaluate(self.input[var.name])
+                return Variable(var, value, membership_value)
+        return None
+
+    def add_input(self, var, value):
+        """
+        Establece el valor de la variable de entrada 'var' como 'value'
+
+        Ejemplo:
+        - Calidad: 3
+        - Servicio: 8
+        - ...
+        """
+        self.input[var] = value
