63cec168b2488f916ee8978cb9bebb0cbcd68223
Oscar Mederos Oceja <omederos@gmail.com>
2012-05-26 22:48:57 -0400

    Add class 'InputParser' that will parse the input file

--- fis/parser.py
+++ fis/parser.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 import re
+from fis.definitions import (VariableCollection, VariableDefinition,
+                             ValueDefinition)
+from fis.functions import Point, TriangularFunction, TrapezoidalFunction
 
 
 class RuleEvaluator(object):
@@ -46,3 +49,123 @@ class RuleParser(object):
         head = head.replace('not(', '-(')
         pattern = re.compile('([A-Z]\w*)\s+=\s+([A-Z]\w*)')
         return pattern.sub(self._replace, head)
+
+
+class InputParser(object):
+    """
+    Parsea un texto o fichero de entrada
+
+    El texto debe tener el siguiente formato:
+
+    # Varias variables de entrada
+    input: (Var1) (Value1) (triangulo/trapecio: (x,y) (x,y) ...)
+    input: (Var1) (Value2) (triangulo/trapecio: (x,y) (x,y) ...)
+    input: (Var1) (Value3) (triangulo/trapecio: (x,y) (x,y) ...)
+    ...
+    input: (Var2) (Value1) (triangulo/trapecio: (x,y) (x,y) ...)
+    input: (Var2) (Value2) (triangulo/trapecio: (x,y) (x,y) ...)
+    input: (Var3) (Value3) (triangulo/trapecio: (x,y) (x,y) ...)
+
+    # Una sola variable de salida (Var1)
+    output: (output_Var) (output_Value1) (triangulo/trapecio: (x,y) (x,y) ...)
+    output: (output_Var) (output_Value3) (triangulo/trapecio: (x,y) (x,y) ...)
+    output: (output_Var) (output_Value1) (triangulo/trapecio: (x,y) (x,y) ...)
+
+    # Reglas. Ejemplos de reglas:
+    rule: Var1 = Value1 => output_Value1
+    rule: not(Var1 = Value1) => output_Value2
+    rule: Var1 = Value1 and Var2 = Value2 => output_Value3
+    rule: Var1 = Value1 or Var2 = Value2 => output_Value1
+    rule: (Var1 = Value1 or Var2 = Value2) and Value2 = Var1 => output_Value3
+    rule: not(Var1 = Value1 or Var2 = Value2) => output_Value2
+    ...
+
+    # Valores iniciales de las variables:
+    ini: Var1 = 10
+    ini: Var2 = 20
+    ...
+
+    """
+    def parse(self, text):
+        # Si es un fichero, leemos su contenido
+        if hasattr(text, 'read'):
+            text = text.read()
+
+        # Parseando las variables de entrada
+        input_vars = self.parse_input_vars(text)
+
+        # Parseando la variable de salida
+        output_var = self.parse_output_var(text)
+
+    def parse_input_vars(self, text):
+        pattern = re.compile(r'input: \((\w+)\) \((\w+)\) \((.*)\)')
+        m = pattern.search(text)
+
+        d = {}
+
+        while m:
+            name = m.group(1)
+            value_raw = m.group(2)
+            func = self.parse_function(m.group(3))
+
+            value = ValueDefinition(value_raw, func)
+            if not name in d:
+                d[name] = []
+
+            d[name].append(value)
+
+            m = pattern.search(text, m.end())
+
+        variables = VariableCollection()
+
+        for name, values in d.items():
+            variables.append(VariableDefinition(name, values))
+
+        return variables
+
+    def parse_function(self, text):
+        m = re.match('(triangulo|trapecio): (.*)', text)
+        if not m:
+            raise Exception('Error parseando la funcion: %s' % text)
+
+        func_type = m.group(1)
+        points_raw = m.group(2)
+
+        points = []
+
+        pattern = re.compile(r'\(([\d\.]+),\s*([\d\.]+)\)')
+        m = pattern.search(points_raw)
+
+        while m:
+            points.append(Point(float(m.group(1)), float(m.group(2))))
+            m = pattern.search(points_raw, m.end())
+
+        if func_type == 'triangulo':
+            cls = TriangularFunction
+        else:
+            cls = TrapezoidalFunction
+        return cls(*points)
+
+    def parse_output_var(self, text):
+        pattern = re.compile(r'output: \((\w+)\) \((\w+)\) \((.*)\)')
+        m = pattern.search(text)
+
+        d = {}
+
+        while m:
+            name = m.group(1)
+            value_raw = m.group(2)
+            func = self.parse_function(m.group(3))
+
+            value = ValueDefinition(value_raw, func)
+            if not name in d:
+                d[name] = []
+
+            d[name].append(value)
+
+            m = pattern.search(text, m.end())
+
+        name = d.items()[0][0]
+        values = d.items()[0][1]
+
+        return VariableDefinition(name, values)
--- tests/parsing.py
+++ tests/parsing.py
@@ -2,8 +2,8 @@
 import unittest
 from fis.definitions import (VariableDefinition, VariableCollection,
                              ValueDefinition)
-from fis.functions import TriangularFunction, Point
-from fis.parser import RuleEvaluator
+from fis.functions import TriangularFunction, Point, TrapezoidalFunction
+from fis.parser import RuleEvaluator, InputParser
 
 
 class ParserTests(unittest.TestCase):
@@ -107,3 +107,73 @@ class EvaluatorTests(unittest.TestCase):
         result = self.evaluator.evaluate(self.variables,
             'not(Agua = Tibia)')
         self.assertEqual(result, 1)
+
+
+class InputParserTests(unittest.TestCase):
+    def setUp(self):
+        self.parser = InputParser()
+
+    def test_parse_function_triangular(self):
+        result = self.parser.parse_function('triangulo: (1,2) (3,4) (5,6)')
+        self.assertIsInstance(result, TriangularFunction)
+        self.assertEqual(result.a, Point(1, 2))
+        self.assertEqual(result.b, Point(3, 4))
+        self.assertEqual(result.c, Point(5, 6))
+
+    def test_parse_function_trapezoidal(self):
+        result = self.parser.parse_function(
+            'trapecio: (1,2) (3,4) (5,6) (7,8)')
+        self.assertIsInstance(result, TrapezoidalFunction)
+        self.assertEqual(result.a, Point(1, 2))
+        self.assertEqual(result.b, Point(3, 4))
+        self.assertEqual(result.c, Point(5, 6))
+        self.assertEqual(result.d, Point(7, 8))
+
+    def test_parse_input_vars(self):
+        result = self.parser.parse_input_vars(
+            """
+            input: (Calidad) (Buena) (triangulo: (1,2) (3,4) (5,6))
+            input: (Calidad) (Regular) (trapecio: (1,2) (3,4) (5,6) (7,8))
+            input: (Calidad) (Mala) (trapecio: (1,2) (3,4) (5,6) (7,8))
+
+            input: (Comida) (Buena) (triangulo: (1,2) (3,4) (5,6))
+            input: (Comida) (Regular) (trapecio: (1,2) (3,4) (5,6) (7,8))
+            input: (Comida) (Mala) (trapecio: (1,2) (3,4) (5,6) (7,8))
+            """
+        )
+        self.assertIsInstance(result, VariableCollection)
+        self.assertEqual(len(result), 2)
+
+        calidad = result[0]
+
+        self.assertEqual(len(calidad.values), 3)
+        self.assertEqual(calidad.values[0].value, 'Buena')
+        self.assertIsInstance(calidad.values[0].function, TriangularFunction)
+        self.assertEqual(calidad.values[1].value, 'Regular')
+        self.assertIsInstance(calidad.values[1].function, TrapezoidalFunction)
+        self.assertEqual(calidad.values[2].value, 'Mala')
+        self.assertIsInstance(calidad.values[2].function, TrapezoidalFunction)
+
+        comida = result[1]
+        self.assertEqual(len(comida.values), 3)
+        self.assertEqual(comida.values[0].value, 'Buena')
+        self.assertIsInstance(comida.values[0].function, TriangularFunction)
+        self.assertEqual(comida.values[1].value, 'Regular')
+        self.assertIsInstance(comida.values[1].function, TrapezoidalFunction)
+        self.assertEqual(comida.values[2].value, 'Mala')
+        self.assertIsInstance(comida.values[2].function, TrapezoidalFunction)
+
+    def test_parse_output_var(self):
+        calidad = self.parser.parse_output_var(
+            """
+            output: (Calidad) (Buena) (triangulo: (1,2) (3,4) (5,6))
+            output: (Calidad) (Regular) (trapecio: (1,2) (3,4) (5,6) (7,8))
+            """
+        )
+        self.assertIsInstance(calidad, VariableDefinition)
+
+        self.assertEqual(len(calidad.values), 2)
+        self.assertEqual(calidad.values[0].value, 'Buena')
+        self.assertIsInstance(calidad.values[0].function, TriangularFunction)
+        self.assertEqual(calidad.values[1].value, 'Regular')
+        self.assertIsInstance(calidad.values[1].function, TrapezoidalFunction)
